02f163c86e50d04c3c0e9f72bd1c9fd82d402a80
rliukshyn <roma.liukshyn@gmail.com>
2016-10-08 00:33:27 +0300

    module4 submission

---
+++ module4/src/categories.controller.js
@@ -0,0 +1,14 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('CategoriesListController', CategoriesListController);
+
+
+CategoriesListController.$inject = ['categories'];
+function CategoriesListController(categories) {
+  var categoriesList = this;
+  categoriesList.categories = categories;
+}
+
+})();
---
+++ module4/src/routes.js
@@ -0,0 +1,46 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/templates/home.template.html'
+  })
+
+  .state('categoriesList', {
+    url: '/categories',
+    templateUrl: 'src/templates/categories.template.html',
+    controller: 'CategoriesListController as categoriesList',
+    resolve: {
+      categories: ['MenuDataService', function (MenuDataService) {
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  })
+
+  .state('items', {
+    url: '/categories/{categoryName}',
+    templateUrl: 'src/templates/items.template.html',
+    controller: 'CategoryItemsController as categoryItems',
+    resolve: {
+      items: ['$stateParams', 'MenuDataService',
+            function ($stateParams, MenuDataService) {
+              return MenuDataService.getItemsForCategory($stateParams.categoryName);
+            }]
+    }
+  });
+}
+
+})();
